f33a46be33c36c27a72479a71d3034970e3eaa97
heronhaye <surya@keyba.se>
2019-04-25 11:19:39 -0400

    fix go vet for 1.12.4

--- run-go-vet.sh
+++ run-go-vet.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e -u -o pipefail # Fail on error
+pkgf=$(mktemp)
 for file in "$@"; do
-    go vet $file
+    (
+        cd "$(dirname "$file")"
+        go list >> "$pkgf"
+    )
+    echo "$file"
 done
+sort -u < "$pkgf" | xargs go vet
